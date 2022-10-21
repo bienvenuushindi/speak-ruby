8af19ab06800e9d4ed2896e54943bba71901df2c
usbbush <jeanbienvenusb@gmail.com>
2022-10-21 17:42:19 +0200

    Implement model for comment entity

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
-class Comment
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+  after_save :update_comments_counter
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
