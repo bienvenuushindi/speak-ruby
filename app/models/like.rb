8b8168756d0732b7d09a1cdf6fe46a325bfcfdf0
usbbush <jeanbienvenusb@gmail.com>
2022-10-21 17:42:32 +0200

    Implement model for like entity

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,8 @@
-class Like
+class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+  after_save :update_likes_counter
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
