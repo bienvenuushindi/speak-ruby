df31a2600c3d90a1df2b4b6878d7c3ec7fe00332
usbbush <jeanbienvenusb@gmail.com>
2022-10-26 12:02:37 +0200

    Add validation to post and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   has_many :posts, inverse_of: 'author'
   has_many :likes, inverse_of: 'author'
   has_many :comments, inverse_of: 'author'
-
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
