a8aa21d69443258907b96db33470da9102561df0
usbbush <jeanbienvenusb@gmail.com>
2022-10-31 17:35:26 +0200

    Implement required changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   get "/users", to: "users#index" # usually requires a view
-  # get "/users/new", to: "users#new" # usually requires a view
   get "/users/:id", to: "users#show" # usually requires a view
   get "/users/:id/posts/:post_id", to: "posts#show" # single post for user id
-  get "/users/:id/posts", to: "users#show_posts_comments" # single post for user id
-  # post "/users", to: "users#create"  # usually a submitted form
-  # get "/users/:id/edit", to: "users#edit" # usually requires a view
-  # put "/users/:id", to: "users#update" # usually a submitted form
-  # delete "/users/:id", to: "users#destroy" # usually called with a button
+  get "/users/:id/posts", to: "posts#index" # single post for user id
 end
