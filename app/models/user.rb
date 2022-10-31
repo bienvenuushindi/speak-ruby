8a09ff3fd888b3a8fd23d8ec74d45c9e43cfffb2
usbbush <jeanbienvenusb@gmail.com>
2022-10-31 16:29:47 +0200

    Update limit number

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
   def recent_posts
-    posts.order(created_at: :desc).limit(3)
+    posts.order(created_at: :desc).limit(5)
   end
 end
