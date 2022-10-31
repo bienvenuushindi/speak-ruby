557c08c5c293abcc5f57dde5cafe473077807369
usbbush <jeanbienvenusb@gmail.com>
2022-10-31 14:32:56 +0200

    Add routes and action's methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def index
+    
+  end
+  def show
+
+  end
+  def find_post_by
+
+  end
+
+  def find_posts
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/users", to: "users#index" # usually requires a view
+  # get "/users/new", to: "users#new" # usually requires a view
+  get "/users/:id", to: "users#show" # usually requires a view
+  get "/users/:id/posts/:post_id", to: "users#find_post_by" # single post for user id
+  get "/users/:id/posts", to: "users#find_posts" # single post for user id
+  # post "/users", to: "users#create"  # usually a submitted form
+  # get "/users/:id/edit", to: "users#edit" # usually requires a view
+  # put "/users/:id", to: "users#update" # usually a submitted form
+  # delete "/users/:id", to: "users#destroy" # usually called with a button
 end
