6b5aee4674be3c00ec38eb80fdeab67257c4d4a5
usbbush <jeanbienvenusb@gmail.com>
2022-11-03 16:43:26 +0200

    Add new aliases

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/users", to: "users#index" # usually requires a view
-  get "/users/:id", to: "users#show" # usually requires a view
+  get "/users", to: "users#index", as: 'users' # usually requires a view
+  get "/users/:id", to: "users#show", as: 'user' # usually requires a view
   get "/users/:id/posts/new", to: "posts#new", as: 'new_post'# new post for user id
   get "/users/:id/posts/:post_id", to: "posts#show", as: 'single_post' # single post for user id
   post "/users/:id/posts", to: "posts#create" # create single post for user id
