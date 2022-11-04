a3e96bd6fc264839e73b9d5d77b19d7b7d940a11
usbbush <jeanbienvenusb@gmail.com>
2022-11-04 10:17:18 +0200

    Update likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,10 +2,10 @@ class LikesController < ApplicationController
   def create
     like = Post.find(params[:post_id]).likes.new(author: current_user)
     if like.save
-      flash[:success] = 'Post saved successfully'
+      flash[:success] = 'Like saved successfully'
     else
       # error message
-      flash.now[:error] = 'Error: Post could not be saved'
+      flash.now[:error] = 'Error: Like could not be saved'
     end
     redirect_back(fallback_location: users_path)
   end
