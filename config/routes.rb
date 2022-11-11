c0c931e03be4a7e7a006c8d46bbb5dd46e6039ac
usbbush <jeanbienvenusb@gmail.com>
2022-11-11 14:52:58 +0200

    Add new route for post delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/", to: "users#index" # usually requires a view
+  root 'users#index'
   get "/users", to: "users#index", as: 'users' # usually requires a view
   get "/users/:id", to: "users#show", as: 'user' # usually requires a view
   get "/users/:id/posts/new", to: "posts#new", as: 'new_post'# new post for user id
-  get "/users/:id/posts/:post_id", to: "posts#show", as: 'single_post' # single post for user id
+  get "posts/:post_id", to: "posts#show", as: 'single_post' # single post for user id
+  delete "posts/:post_id", to: "posts#destroy" # single post for user id
   post "/users/:id/posts", to: "posts#create" # create single post for user id
   get "/users/:id/posts", to: "posts#index", as: 'user_posts' # single post for user id
   get "/users/:id/posts/:post_id/comments/new", to: "comments#new", as: 'new_comment'
